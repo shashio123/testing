24af7e2233a2e24ee56af4084b034b28461a0976
Shashikumara ontigodi <147982297+shashio123@users.noreply.github.com>
2023-12-18 14:39:13 +0000

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,70 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def draw_cat_plot():
+    # Read data
+    df = pd.read_csv('medical_examination.csv')
+
+    # Add 'overweight' column
+    df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
+
+    # Normalize data
+    df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+    df['gluc'] = (df['gluc'] > 1).astype(int)
+
+    # Clean data
+    df = df[(df['ap_lo'] <= df['ap_hi']) &
+            (df['height'] >= df['height'].quantile(0.025)) &
+            (df['height'] <= df['height'].quantile(0.975)) &
+            (df['weight'] >= df['weight'].quantile(0.025)) &
+            (df['weight'] <= df['weight'].quantile(0.975))]
+
+    # Create 'cardio' column
+    df['cardio'] = df['cardio'].astype(int)
+
+    # Melt DataFrame
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active'])
+
+    # Draw the catplot
+    g = sns.catplot(data=df_cat, kind='count', x='variable', hue='value', col='cardio')
+    g.set_axis_labels('variable', 'total')
+    plt.show()
+
+def draw_heat_map():
+    # Read data
+    df = pd.read_csv('medical_examination.csv')
+
+    # Add 'overweight' column
+    df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
+
+    # Normalize data
+    df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+    df['gluc'] = (df['gluc'] > 1).astype(int)
+
+    # Clean data
+    df = df[(df['ap_lo'] <= df['ap_hi']) &
+            (df['height'] >= df['height'].quantile(0.025)) &
+            (df['height'] <= df['height'].quantile(0.975)) &
+            (df['weight'] >= df['weight'].quantile(0.025)) &
+            (df['weight'] <= df['weight'].quantile(0.975))]
+
+    # Calculate the correlation matrix
+    corr = df.corr()
+
+    # Generate a mask for the upper triangle
+    mask = ~pd.np.tri(corr.shape[0], k=-1, dtype=bool)
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', cmap='coolwarm', vmax=0.3, center=0, square=True,
+                linewidths=0.5, cbar_kws={"shrink": 0.8})
+
+    plt.show()
+
+# Call the functions
+draw_cat_plot()
+draw_heat_map()
 
